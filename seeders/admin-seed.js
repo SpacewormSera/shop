8f0d699de263431a0227cbcf8fc2b498e18d7ca8
spacewormsera <deluxe87@mail.ru>
2021-10-06 16:09:40 +0300

    initial

--- seeders/admin-seed.js
+++ seeders/admin-seed.js
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose';
-import Admin from '../models/admin.js';
+import mongoose from "mongoose";
+import Admin from "../models/admin.js";
 
-mongoose.connect(`mongodb+srv://anbochkarev1991:temporarypassword12345@cluster0-glx7r.mongodb.net/DonCarapuldon?retryWrites=true&w=majority`, {
+mongoose.connect(`your uri`, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 async function adminSeed() {
   const admin = new Admin({
-    login: 'ksenia_gayvoronovskaya',
-    password: 'temporary-admin-password',
+    login: "ksenia_gayvoronovskaya",
+    password: "temporary-admin-password",
   });
   await admin.save();
   mongoose.disconnect();
--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
-// Основной файл со всеми ручками и подключениями (подключения не совсем все)
-import express from 'express';
-import createError from 'http-errors';
-import path from 'path';
-import dotenv from 'dotenv';
+// Основной файл со всеми роутами и подключениями (подключения не совсем все)
+import express from "express";
+import createError from "http-errors";
+import path from "path";
+import dotenv from "dotenv";
 
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import sessionFileStore from 'session-file-store';
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import sessionFileStore from "session-file-store";
 
-import indexRouter from '../routes/index.js';
-import items1Router from '../routes/items1.js';
-import items2Router from '../routes/items2.js';
-import authRouter from '../routes/auth.js';
-import adminPanelRouter from '../routes/adminPanel.js';
-import authChangeRouter from '../routes/auth-change.js';
-import addDataRouter from '../routes/addData.js';
+import indexRouter from "../routes/index.js";
+import items1Router from "../routes/items1.js";
+import items2Router from "../routes/items2.js";
+import authRouter from "../routes/auth.js";
+import adminPanelRouter from "../routes/adminPanel.js";
+import authChangeRouter from "../routes/auth-change.js";
+import addDataRouter from "../routes/addData.js";
 
 const FileStore = sessionFileStore(session);
 
@@ -27,28 +27,30 @@ mongoose.connect(process.env.DB_ACCESS, {
   useUnifiedTopology: true,
 });
 
-app.set('view engine', 'hbs');
-app.set('views', path.resolve('views'));
+app.set("view engine", "hbs");
+app.set("views", path.resolve("views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(cookieParser());
-app.use(session({
-  store: new FileStore(),
-  key: 'admin_sid',
-  secret: 'Don Carapuldon',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    expires: 600000000000,
-  },
-}));
+app.use(
+  session({
+    store: new FileStore(),
+    key: "admin_sid",
+    secret: "Don Carapuldon",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 600000000000,
+    },
+  })
+);
 
-app.use('/', indexRouter);
-app.use('/items1', items1Router);
-app.use('/items2', items2Router);
+app.use("/", indexRouter);
+app.use("/items1", items1Router);
+app.use("/items2", items2Router);
 
 app.use((req, res, next) => {
   res.locals.isAuth = !!req.session.admin;
@@ -58,10 +60,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/auth', authRouter);
-app.use('/admin', adminPanelRouter);
-app.use('/new-data', authChangeRouter);
-app.use('/add', addDataRouter);
+app.use("/auth", authRouter);
+app.use("/admin", adminPanelRouter);
+app.use("/new-data", authChangeRouter);
+app.use("/add", addDataRouter);
 
 // Отлавливаем 404 ошибки
 app.use((req, res, next) => next(createError(404)));
@@ -69,9 +71,9 @@ app.use((req, res, next) => next(createError(404)));
 // Рендерим страницу ошибки
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 export default app;
